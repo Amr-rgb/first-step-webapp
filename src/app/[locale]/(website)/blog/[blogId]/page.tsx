ef784c08b1f48698d552415e405848ec85802669
Amr Taha <amrshiha2@gmail.com>
2025-07-06 10:47:27 +0300

    feat(blog): add wave illustration and enhance blog title styling

--- src/app/[locale]/(website)/blog/[blogId]/page.tsx
+++ src/app/[locale]/(website)/blog/[blogId]/page.tsx
@@ -25,16 +25,42 @@ export default async function BlogPage({
           fill
           priority
         />
+        <div className="absolute scale-105 -bottom-0.5 -left-0.5 w-full  overflow-hidden">
+          <Image
+            src="/assets/illustrations/wave.svg"
+            alt="Wave decoration"
+            width={1440}
+            height={610}
+            className="w-full h-full object-cover"
+          />
+        </div>
       </div>
 
       <div className="mt-9 container px-4 mx-auto flex flex-col gap-6 lg:flex-row xl:gap-12 2xl:gap-14.5">
         <div className="flex-[3]">
           <div className="prose prose-lg mx-auto">
-            <h1 className="text-2xl prose prose-lg">
-              {typeof blog.title === "string"
-                ? blog.title
-                : blog.title?.[locale]}
-            </h1>
+            <div className="mb-8">
+              <h1 className="mb-1.5 text-3xl capitalize font-normal">
+                {typeof blog.title === "string"
+                  ? blog.title
+                  : blog.title?.[locale]}
+              </h1>
+
+              <p className="!my-0 text-mid-gray">
+                <span>
+                  {new Date(blog.published_at).toLocaleDateString(locale, {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                  })}
+                </span>{" "}
+                —{" "}
+                <span>
+                  {blog.reading_time} {t("minutes")}
+                </span>
+              </p>
+            </div>
+
             <div
               className="prose prose-lg mx-auto"
               dangerouslySetInnerHTML={{ __html: blog.content?.[locale] }}
