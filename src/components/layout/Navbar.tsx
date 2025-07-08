e506ba79ad88f6120826b01e39c16cd29311fed9
Amr Taha <amrshiha2@gmail.com>
2025-07-08 14:42:16 +0300

    fix: buttons z index issue

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -44,7 +44,7 @@ const Navbar = ({ children }: { children?: React.ReactNode }) => {
   return (
     <div className="relative container mx-auto px-4 py-2.5">
       {!token && (
-        <div className="z-10 fixed top-64 -left-[90px] -rotate-90 flex items-center gap-x-4">
+        <div className="z-[9999] fixed top-64 -left-[90px] -rotate-90 flex items-center gap-x-4">
           <Button
             asChild
             size={"sm"}
