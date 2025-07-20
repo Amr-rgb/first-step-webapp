b461cf0ec7b883f8c6975f795d1e838d2bfe7037
Amr Taha <amrshiha2@gmail.com>
2025-07-20 10:17:08 +0300

    fix(api): add Accept header to newsletter service for JSON responses

--- src/services/api.ts
+++ src/services/api.ts
@@ -267,6 +267,7 @@ export const websiteService = {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            Accept: "application/json",
             "X-Authorization": process.env.NEXT_PUBLIC_X_AUTHORIZATION || "",
             "X-Authorization-Secret":
               process.env.NEXT_PUBLIC_X_AUTHORIZATION_SECRET || "",
