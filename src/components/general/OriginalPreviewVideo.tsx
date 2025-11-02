93e652a4068a9d470745a098a51e18c4fdd4826b
Omnia Arafat <83679206+Omnia-Arafat@users.noreply.github.com>
2025-11-02 15:58:49 +0200

    feat(website): add OriginalPreviewVideo component to the Our Story page and update PreviewVideo component to use an iframe for video display

---
+++ src/components/general/OriginalPreviewVideo.tsx
@@ -0,0 +1,36 @@
+import MuxPlayer from "@mux/mux-player-react";
+import { createBlurUp } from "@mux/blurup";
+
+const options = {};
+const muxPlaybackId = "T8It02oFFSo401wcc00tTcn4WCbRW3CjO1qE0202ZyuBexLk";
+
+const OriginalPreviewVideo = async () => {
+  const { blurDataURL, aspectRatio } = await createBlurUp(
+    muxPlaybackId,
+    options
+  );
+
+  return (
+    <section className="container mx-auto px-4">
+      <div
+        className="overflow-hidden flex rounded-5xl border border-secondary-burgundy lg:mx-10"
+        style={{ aspectRatio }}
+      >
+        <MuxPlayer
+          playbackId={muxPlaybackId}
+          metadata={{
+            video_id: "5CtgfRTknowfMGd00gzeKiPIEhcXJ02DJppj9Doy1vUNk",
+            video_title: "First Step Preview",
+            viewer_user_id: "user-id-007",
+          }}
+          accentColor="#D9534F"
+          placeholder={blurDataURL}
+          style={{ aspectRatio }}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default OriginalPreviewVideo;
+
