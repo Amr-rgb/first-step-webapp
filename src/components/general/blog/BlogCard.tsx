baf09d9b51ec65595cdcd5604a965229d61c0e74
Amr Taha <amrshiha2@gmail.com>
2025-11-10 14:17:04 +0200

    refactor(blogs): optimize blog styling

--- src/components/general/blog/BlogCard.tsx
+++ src/components/general/blog/BlogCard.tsx
@@ -3,13 +3,17 @@ import { Link } from "@/i18n/navigation";
 import { useLocale, useTranslations } from "next-intl";
 import { Icons } from "../icons";
 import { Blog } from "@/types";
+import { Clock } from "lucide-react";
 
 const BlogCard = ({ blog }: { blog: Blog }) => {
   const locale = useLocale();
   const t = useTranslations("blog");
 
   return (
-    <div className="bg-white shadow-card min-w-60 p-2 pb-4 flex flex-col items-start gap-y-2 rounded-2xl text-left rtl:text-right">
+    <Link
+      href={`/blog/${blog.id}`}
+      className="bg-white shadow-card min-w-60 p-2 pb-4 flex flex-col items-start gap-y-2 rounded-2xl text-left rtl:text-right hover:shadow-sm transition-shadow duration-300 cursor-pointer"
+    >
       <div className="w-full h-40 rounded-xl overflow-hidden relative">
         <Image
           src={blog.image}
@@ -23,9 +27,9 @@ const BlogCard = ({ blog }: { blog: Blog }) => {
         />
       </div>
 
-      <Link href={`/blog/${blog.id}`} className="text-primary font-bold">
+      <p className="text-primary font-bold">
         {typeof blog.title === "string" ? blog.title : blog.title?.[locale]}
-      </Link>
+      </p>
 
       <p className="text-gray text-sm line-clamp-3">
         {typeof blog.description === "string"
@@ -33,11 +37,25 @@ const BlogCard = ({ blog }: { blog: Blog }) => {
           : blog.description?.[locale]}
       </p>
 
-      <div className="w-full flex items-end justify-between text-sm">
-        <div className="flex flex-col gap-y-2">
-          <span className="text-secondary-orange font-medium text-sm">
-            {blog.reading_time} {t("minutes")}
+      <div className="mt-auto w-full flex items-end justify-between text-sm">
+        <div className="flex items-center gap-x-0.5">
+          <Icons.calendar className="fill-gray text-gray size-4" />
+          <span className="font-medium text-gray">
+            {new Date(blog.published_at).toLocaleDateString(locale, {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            })}
           </span>
+        </div>
+
+        <div className="flex flex-col gap-y-2">
+          <div className="flex items-center gap-x-0.5">
+            <Clock className="text-info size-4" />
+            <span className="text-info font-medium text-sm">
+              {blog.reading_time} {t("minutes")}
+            </span>
+          </div>
 
           {blog.author && (
             <div className="flex items-center gap-x-1">
@@ -49,19 +67,8 @@ const BlogCard = ({ blog }: { blog: Blog }) => {
             </div>
           )}
         </div>
-
-        <div className="flex items-center gap-x-0.5">
-          <Icons.calendar className="fill-gray" width={12} height={12} />
-          <span className="font-medium">
-            {new Date(blog.published_at).toLocaleDateString(locale, {
-              year: "numeric",
-              month: "short",
-              day: "numeric",
-            })}
-          </span>
-        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
