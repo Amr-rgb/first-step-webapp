224b3dd5baedc437f99b16b572480531f7b3a59e
Amr Taha <amrshiha2@gmail.com>
2025-12-22 22:10:00 +0200

    re-add gate item in sidebar

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -67,11 +67,11 @@ const getCenterNavbar = (t: any): NavbarItem[] => [
     url: "/dashboard/center/center-data",
     icon: dashboardIcons.site,
   },
-  // {
-  //   title: t("center.gate"),
-  //   url: "/dashboard/center/gate",
-  //   icon: dashboardIcons.site,
-  // },
+  {
+    title: t("center.gate"),
+    url: "/dashboard/center/gate",
+    icon: dashboardIcons.site,
+  },
   {
     title: t("center.ad-or-blog-request"),
     url: "/dashboard/center/ad-or-blog-request",
